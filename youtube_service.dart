ee31f4e2063d449f6a17b0b02c411575e2717e84
rosticom <ros10@meta.ua>
2022-04-13 14:39:53 +0300

    youtube_service

--- youtube_service.dart
+++ youtube_service.dart
@@ -1,16 +1,16 @@
 
-import 'package:topointme/view/widget/video_content/youtube/service/core/youtube_api.dart';
-import 'package:topointme/view/widget/video_content/youtube/youtube_video.dart';
+import '...service/core/youtube_api.dart';
+import '...service/core/youtube_video.dart';
 
 class YoutubeService {
 
-  static final String key = "AIzaSyA2ornWK5NgPEcIL_poBgymseTdxQr4VOg"; // ** OWN API **
-  static final YoutubeAPI ytApi = YoutubeAPI(key, maxResults: 5, type: "playlist");//playlist
-  static final String query = "PLxHGsGbuTY2A2Aauwi84wpQi8eL1DBB6a";//playlist
+  static final String key = "AIzaSyBP38EU0CA45rd2umTRjwOwSQ0qUh-edfr"; // ** OWN YOUTUBE V3 API **
+  static final YoutubeAPI ytApi = YoutubeAPI(key, maxResults: 5, type: "playlist");
+  static final String query = "PLxHGsGbuTY2A2Aauwi84wpQi8eL1DBB6a";//playlist id
 
   static searchResultsYoutube() async {
-    // List<YouTubeVideo> _videoResult = await ytApi.search(query);
-    List<YouTubeVideoPlayList> _videoResult = await ytApi.playList(query);
-    return _videoResult;
+    // List<YouTubeVideo> videoResult = await ytApi.search(query); <- REPLACED WITH NEXT
+    List<YouTubeVideoPlayList> videoResult = await ytApi.playList(query);
+    return videoResult;
   }
 }
