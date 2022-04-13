18fb9b0c5fe61f366750393207e823e01b2a0aaa
rosticom <ros10@meta.ua>
2022-04-13 13:51:58 +0300

    youtube_api^1.0.4_fixed

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Fixed Flutter Pub Lib: youtube_api 1.0.4
 (git) https://github.com/nitishk72/Youtube_API
 
 1. Replace youtube_api.dart
-2. Replace _api.dart with playList_api.dart
+2. Replace youtube_video.dart
+3. Replace _api.dart with playList_api.dart 
 
 Use the playList method instead of search.
 Now it shows YouTube playlist items.
---
+++ core/youtube_video.dart
@@ -0,0 +1,51 @@
+
+
+import 'package:youtube_api/src/model/thumbnails.dart';
+
+class YouTubeVideoPlayList {
+  late Thumbnails thumbnail;
+  String? kind;
+  String? id;
+  String? publishedAt;
+  String? channelId;
+  String? channelUrl;
+  late String title;
+  String? description;
+  late String channelTitle;
+  late String url;
+  String? duration;
+
+  YouTubeVideoPlayList(dynamic data, {bool getTrendingVideo: false}) {
+    thumbnail = Thumbnails.fromMap(data['snippet']['thumbnails']);
+    if (getTrendingVideo) {
+      kind = 'video';
+      id = data['id'];
+    } else {
+      // kind = data['kind'].substring(8);//kind = data['id']['kind'].substring(8);
+      // id = data['id'][data['id'].keys.elementAt(1)];
+      kind = data['snippet']['resourceId']['kind'].substring(8);
+      id = data['snippet']['resourceId']['videoId'];
+    }
+    url = getURL(kind!, id!);
+    publishedAt = data['snippet']['publishedAt'];
+    channelId = data['snippet']['channelId'];
+    channelUrl = "https://www.youtube.com/channel/$channelId";
+    title = data['snippet']['title'];
+    description = data['snippet']['description'];
+    channelTitle = data['snippet']['channelTitle'];
+  }
+
+  String getURL(String kind, String id) {
+    String baseURL = "https://www.youtube.com/";
+    switch (kind) {
+      case 'channel':
+        return "${baseURL}channel/$id";
+      case 'video':
+        return "${baseURL}watch?v=$id";
+      case 'playlist':
+        return "${baseURL}playlist?list=$id";
+    }
+    return baseURL;
+  }
+}
+
